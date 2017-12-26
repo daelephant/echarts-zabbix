d7ff24e3ca6a7bb294c4e5166198f4f9a016e8a9
elephant2 <2224168716@qq.com>
2017-12-26 16:33:33 +0800

    success for ring && brokenLine && （init success demo）

---
+++ controller/brokenLine.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cyx
+ * Date: 2017-12-25
+ * Time: 18:01
+ */
+
+include_once('..\conf\connect.php');
+
+$res = mysql_query("select   
+sum(case month(time) when '1' then 1 else 0 end) as 一月份,  
+sum(case month(time) when '2' then 1 else 0 end) as 二月份,  
+sum(case month(time) when '3' then 1 else 0 end) as 三月份,  
+sum(case month(time) when '4' then 1 else 0 end) as 四月份,  
+sum(case month(time) when '5' then 1 else 0 end) as 五月份,  
+sum(case month(time) when '6' then 1 else 0 end) as 六月份,
+sum(case month(time) when '7' then 1 else 0 end) as 七月份,
+sum(case month(time) when '8' then 1 else 0 end) as 八月份,
+sum(case month(time) when '9' then 1 else 0 end) as 九月份,
+sum(case month(time) when '10' then 1 else 0 end) as 十月份,
+sum(case month(time) when '11' then 1 else 0 end) as 十一月份,
+sum(case month(time) when '12' then 1 else 0 end) as 十二月份
+from `event`  
+where year(time)='2017';");
+
+while($row = mysql_fetch_assoc($res)){
+
+    //var_dump($row);//结果是Array
+    //$arrk = [];
+    //$arrv = [];
+
+    foreach ($row as $k => $v){
+
+        //array_push($arrk,$k);
+        //array_push($arrv,$v);
+        $arr[] = array(
+            "name" =>  $k,
+            "value" => intval($v)
+        );
+    }
+
+}
+//var_dump($arr);
+//var_dump($arrk);
+$ring = json_encode($arr);
+//$ring2 = json_encode($arrv);
+echo $ring;
+//echo $ring2;
+?>
